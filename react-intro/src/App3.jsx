3a8191e706ea12df4da2e66a783dc213056c230f
Roshani2211205 <roshani2211205@iimscollege.edu.np>
2025-01-29 12:14:47 +0545

    Update

--- react-intro/src/App3.jsx
+++ react-intro/src/App3.jsx
@@ -1,22 +1,22 @@
-// import React from "react";
+import React from "react";
 
-// const DisplaySum = (props) => {
-//     const sum= props.x +props.y;
-//       return (
-//     <div>
-//       <h1>{sum}</h1>
-//     </div>
-//   );
-// };
+const DisplaySum = (props) => {
+    const sum= props.x +props.y;
+      return (
+    <div>
+      <h1>{sum}</h1>
+    </div>
+  );
+};
 
-// const App3 = () => {
-//   return (
-//     <div>
-//       <DisplaySum x={2} y={3} />
-//       <DisplaySum x={20} y={30} />
-//       <DisplaySum x={220} y={330} />
-//     </div>
-//   );
-// };
+const App3 = () => {
+  return (
+    <div>
+      <DisplaySum x={2} y={3} />
+      <DisplaySum x={20} y={30} />
+      <DisplaySum x={220} y={330} />
+    </div>
+  );
+};
 
-// export default App3;
+export default App3;
